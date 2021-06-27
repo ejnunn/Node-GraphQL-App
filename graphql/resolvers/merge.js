90569ec3dda4f11d9fbab51bd9369469ae9baadd
Eric Nunn <ericnunn@Erics-MacBook-Pro.local>
2021-06-26 20:04:44 -0700

    finished dataloader implementation for all types of data being fetched. Reduces multiple roundtrips to the database.

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,7 +1,20 @@
+const DataLoader = require('dataloader');
+
 const Event = require('../../models/event');
 const User = require('../../models/user');
-const Message = require('../../models/message');
 const { dateToString } = require('../../helpers/date');
+
+const eventLoader = new DataLoader((eventIds) => {
+  return events(eventIds);
+});
+
+const userLoader = new DataLoader(userIds => {
+  return User.find({_id: {$in: userIds}});
+});
+
+const messageLoader = new DataLoader(messageIds => {
+  return Message.find({_id: {$in: messageIds}});
+});
 
 const events = async eventIds => {
   try {
@@ -16,8 +29,8 @@ const events = async eventIds => {
 
 const singleEvent = async eventId => {
   try {
-    const event = await Event.findById(eventId);
-    return transformEvent(event);
+    const event = await eventLoader.load(eventId.toString());
+    return event;
   } catch (err) {
     throw err;
   }
@@ -25,11 +38,11 @@ const singleEvent = async eventId => {
 
 const user = async userId => {
   try {
-    const user = await User.findById(userId);
+    const user = await userLoader.load(userId.toString());
     return {
       ...user._doc,
       _id: user.id,
-      createdEvents: events.bind(this, user._doc.createdEvents)
+      createdEvents: () => eventLoader.loadMany(user._doc.createdEvents)
     };
   } catch (err) {
     throw err;
@@ -58,7 +71,7 @@ const transformBooking = booking => {
 
 const messages = async messageIds => {
   try {
-    const messages = await Message.find({ _id: { $in: messageIds } });
+    const messages = await messageLoader.load(messageIds);
     return messages.map(message => {
       return transformMessage(message);
     });
@@ -77,6 +90,8 @@ const transformMessage = message => {
     body: message.body
   };
 };
+
+
 
 const transformUser = user => {
   return {
